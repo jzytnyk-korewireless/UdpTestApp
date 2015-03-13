1b1e72d3af3533a352f3efa98e4ce5babe9fa857
msaini-korewireless <msaini@koresystemsgroup.com>
2015-03-13 15:58:48 -0500

    fdfd

---
+++ UdpTestApp/UdpTestApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace UdpTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_SendUdpPacket = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_SendUdpPacket
+            // 
+            this.btn_SendUdpPacket.Location = new System.Drawing.Point(68, 78);
+            this.btn_SendUdpPacket.Name = "btn_SendUdpPacket";
+            this.btn_SendUdpPacket.Size = new System.Drawing.Size(126, 48);
+            this.btn_SendUdpPacket.TabIndex = 0;
+            this.btn_SendUdpPacket.Text = "Send UDP Packet";
+            this.btn_SendUdpPacket.UseVisualStyleBackColor = true;
+            this.btn_SendUdpPacket.Click += new System.EventHandler(this.btn_SendUdpPacket_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 255);
+            this.Controls.Add(this.btn_SendUdpPacket);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_SendUdpPacket;
+    }
+}
+
---
+++ UdpTestApp/UdpTestApp/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UdpTestApp
+{
+    public partial class Form1 : Form
+    {
+        private Socket _radiusSocket;
+        private const int SIXTEEN_MEGABYTES = 16777216;
+        private IPAddress _radiusServerIp = IPAddress.Any;
+        private int _radiusPort = 1812;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SendUdpPacket_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (_radiusSocket != null)
+                {
+                    _radiusSocket.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            try
+            {
+                _radiusSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                _radiusSocket.SendBufferSize = SIXTEEN_MEGABYTES;
+                _radiusSocket.ReceiveBufferSize = SIXTEEN_MEGABYTES;
+                //_radiusSocket.Bind(new IPEndPoint(_radiusServerIp, _radiusPort));
+
+                //uint IOC_IN = 0x80000000;
+                //uint IOC_VENDOR = 0x18000000;
+                //uint SIO_UDP_CONNRESET = IOC_IN | IOC_VENDOR | 12;
+
+                string testString = "Giggle";
+
+                IPAddress serverAddr = IPAddress.Parse("172.31.100.156");
+
+                IPEndPoint endPoint = new IPEndPoint(serverAddr, 1812);
+
+                var sendData = Encoding.Default.GetBytes(testString);
+
+                // _radiusSocket.IOControl((int)SIO_UDP_CONNRESET, new[] { Convert.ToByte(false) }, null);
+
+                _radiusSocket.SendTo(sendData, 0, testString.Length, SocketFlags.None, endPoint);
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+        }
+
+    }
+}
